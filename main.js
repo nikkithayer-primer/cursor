70db31d5dcebc4d437ded36d016e5dc7041129ea
nikkithayer-primer <nicole.thayer@primer.ai>
2025-06-30 15:21:08 -0700

    Styling layer count

--- main.js
+++ main.js
@@ -225,10 +225,13 @@ document.addEventListener('DOMContentLoaded', function() {
             layerHeader.className = 'layer-header';
             layerHeader.innerHTML = `
                 <div class="layer-title">
-                    <button class="layer-control-btn target-btn layer-${layerName.toLowerCase().replace('-', '')}" title="Zoom to layer">
-                        <svg viewBox="0 0 24 24"><path d="M9.5,3A6.5,6.5 0 0,1 16,9.5C16,11.11 15.41,12.59 14.44,13.73L14.71,14H15.5L20.5,19L19,20.5L14,15.5V14.71L13.73,14.44C12.59,15.41 11.11,16 9.5,16A6.5,6.5 0 0,1 3,9.5A6.5,6.5 0 0,1 9.5,3M9.5,5C7,5 5,7 5,9.5C5,12 7,14 9.5,14C12,14 14,12 14,9.5C14,7 12,5 9.5,5Z"/></svg>
-                    </button>
-                    <span class="layer-name">${layerName}</span>
+                    <div class="layer-title-row">
+                        <button class="layer-control-btn target-btn layer-${layerName.toLowerCase().replace('-', '')}" title="Zoom to layer">
+                            <svg viewBox="0 0 24 24"><path d="M9.5,3A6.5,6.5 0 0,1 16,9.5C16,11.11 15.41,12.59 14.44,13.73L14.71,14H15.5L20.5,19L19,20.5L14,15.5V14.71L13.73,14.44C12.59,15.41 11.11,16 9.5,16A6.5,6.5 0 0,1 3,9.5A6.5,6.5 0 0,1 9.5,3M9.5,5C7,5 5,7 5,9.5C5,12 7,14 9.5,14C12,14 14,12 14,9.5C14,7 12,5 9.5,5Z"/></svg>
+                        </button>
+                        <span class="layer-name">${layerName}</span>
+                    </div>
+                    <div class="layer-count">${locationsByLayer[layerName].length} points</div>
                 </div>
                 <div class="layer-controls">
                     <button class="layer-control-btn eye-toggle" title="Toggle visibility">
